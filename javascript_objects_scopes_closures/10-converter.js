ff5b55238ef88f64d141b472d0050e8161ef0e8d
Emmanuella00 <e.ikirezi@alustudent.com>
2025-05-31 04:03:34 -0700

    java object

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-exports.converter = function (base) { return (num) => num.toSstring(base); };
+exports.converter = function (base) {
+  return function f (n, s) {
+    if (s === undefined) s = '';
+    if (n === 0) {
+      return s === '' ? '0' : s;
+    }
+    return f(Math.floor(n / base), (n % base).toString(base) + s);
+  };
+};
