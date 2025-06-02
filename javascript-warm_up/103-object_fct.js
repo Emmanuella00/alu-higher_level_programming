d7536f971b0aa8a94e8128b3b0dba3fee2499cc1
Emmanuella00 <e.ikirezi@alustudent.com>
2025-06-02 03:07:37 -0700

    javascript warmup

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
 // This script checks for command line arguments and prints a message based on their presence
-const argv = process.argv.slice(2);
-
 const myObject = {
-  value: parseInt(argv[1], 10) || 0,
+  type: 'object',
+  value: 12,
   incr: function () {
     this.value++;
   }
 };
 
-console.log(`${argv[0]} is ${myObject.value}`);
+console.log(myObject);
 myObject.incr();
-console.log(`After increment: ${argv[0]} is ${myObject.value}`);
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
